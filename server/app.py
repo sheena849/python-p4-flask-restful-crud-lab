da979969c2519977ea2b7c72f23a6b26bc7ef257
sheena849 <sheenamugo04@gmail.com>
2025-01-23 15:10:47 +0300

    Completed lab

--- server/app.py
+++ server/app.py
@@ -46,6 +46,31 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        record =Plant.query.filter(Plant.id==id).first()
+
+        #Parse the JSON payload
+        payload = request.get_json()
+        
+        #Update the record with the data in the payload
+        for attr, value in payload.items():
+            if hasattr(record, attr):
+                setattr(record, attr, value)
+
+        # Commit the changes to database
+        db.session.add(record)
+        db.session.commit()
+        response_dict=record.to_dict()
+        return make_response(response_dict, 200)
+    
+    def delete(self, id):
+        record = Plant.query.filter(Plant.id==id).first()
+        db.session.delete(record)
+        db.session.commit()
+        response_dict=''
+        response =make_response(response_dict, 204)
+        return response
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
